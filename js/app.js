5451dc16aa9f90d8757495b43d3319fb221fec19
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-12-17 02:49:44 +0600

    upload image ans set image in modal with crop done

--- js/app.js
+++ js/app.js
@@ -2,8 +2,34 @@ const imageInputBtn = document.querySelector("#img_input");
 
 const uploadBtn = document.querySelector(".upload_btn");
 
+const modal = document.querySelector(".modal_container");
+const modalImageContainer = document.querySelector(".modal_image_container");
 // active image input button
 
 uploadBtn.addEventListener("click", () => {
   imageInputBtn.click();
+});
+
+imageInputBtn.addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      // const result = reader.result;
+      let img = document.createElement("img");
+      img.id = "image";
+      img.src = e.target.result;
+      // clean result before
+      modalImageContainer.innerHTML = "";
+      // append new image
+      modalImageContainer.appendChild(img);
+      modal.classList.add("active");
+      cropper = new Cropper(img);
+    };
+    // cancelBtn.addEventListener("click", function () {
+    //   img.src = "";
+    //   modal.classList.add("active");
+    // });
+    reader.readAsDataURL(file);
+  }
 });
